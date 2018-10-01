60907d1ca4df54716527856a9f2466918d25f051
danielbonnells <danielbonnells1@gmail.com>
2018-10-01 10:59:01 -0400

    moved takeover spot in html

--- script.js
+++ script.js
@@ -32,8 +32,6 @@ window.addEventListener("resize", function(){
 });
 
 
-
-
 // Testing Fetching of newsroom posts
 let newsroomContainer = document.getElementsByClassName('newsroom')[0]
 
@@ -45,6 +43,7 @@ let newsroomPosts = fetch('https://newsroom.ocfl.net/wp-json/wp/v2/posts?_embed'
 const formatData = (data) => {
   data.map((story,index) => {
     let newsroomContainerContainer = document.createElement('div');
+    // The first story is displayed different than all others
     if(index === 0){
       newsroomContainerContainer.innerHTML = `<div class="newsroom-item highlight-item">
            <h5><a href="${story.link}" target="_blank">${story.title.rendered}</a></h5>
@@ -56,24 +55,14 @@ const formatData = (data) => {
       newsroomContainerContainer.innerHTML = `<div class="newsroom-item">
         <h5><a href="${story.link}" target="_blank">${story.title.rendered}</a></h5>
            </div>`
-    }else {
+    } else {
+      // all items in the response past the 3rd are ignored
      return null; 
+
     }
     
     newsroomContainer.appendChild(newsroomContainerContainer); 
-    
-    console.log(story);
+
   })
   
 }
-
-
-// <div class="newsroom-item highlight-item">
-//         <h5><a href="https://newsroom.ocfl.net/2018/09/orange-county-commissioner-jennifer-thompson-hosts-community-celebration-back-nature-wildlife-refuge/" target="_blank">Orange County Commissioner Jennifer Thompson Hosts Community Celebration at Back to Nature Wildlife Refuge</a></h5>
-//            <div class="newsroom-item-image-container">
-//             <img src="https://newsroom.ocfl.net/wp-content/uploads/2018/09/9-7-18-Back-to-Nature-Wildlife-Refuge-Newsroom-Story-1024x683.jpeg" alt="" />
-//           </div>
-//             <p>Community celebrations are meant to bring residents and officials together to acknowledge projects that have either already been completed or are about to start. On September 7, 2018, a community celebration highlighting future enhancements to the Back to Nature Wildlife Refuge took place at Eagles Roost Park. <span class="newsroom-item-learnmore"><a href="https://newsroom.ocfl.net/2018/09/orange-county-commissioner-jennifer-thompson-hosts-community-celebration-back-nature-wildlife-refuge/">Learn more</a></span></p>
-            
-         
-//       </div>
